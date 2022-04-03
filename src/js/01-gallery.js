c1d2e8d5a1368d7f3ec3092187a1a9e77e43b177
funz1k <92177445+funz1k@users.noreply.github.com>
2022-04-03 16:56:52 +0300

    task - 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,49 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery')
+
+const markup = galleryItems.map(({ preview, original, description }) => {
+    return `
+    <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+    <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+    />
+        </a>
+    </div>
+    `
+}).join('')
+
+galleryList.insertAdjacentHTML('afterbegin', markup);
+
+galleryList.addEventListener("click", onModalOpenImg);
+
+
+function onModalOpenImg(event) {
+    event.preventDefault()
+    if (event.target.nodeName !== 'IMG') {
+        return
+    }
+    const largeImg = event.target.dataset.source
+    createModalWindow(largeImg)
+
+}
+
+function createModalWindow(url) {
+    const instance = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">
+`)
+    const onModalClose = ((event) => {
+        if (event.code === 'Escape') {
+            instance.close()
+        }
+    })
+    instance.show()
+    window.addEventListener('keydown', onModalClose);
+
+}
+
